3a4cab68f59dfd30f02d0aa02b909ffc40a693e5
Thanveerahmd <ahamedthanveer123@gmail.com>
2019-10-04 11:46:44 +0530

    latest

--- Dto/ScoreDto.cs
+++ Dto/ScoreDto.cs
@@ -3,15 +3,14 @@ using System;
 namespace TkdScoringApp.API.Dto
 {
     public class ScoreDto
-    {
-         
+    { 
         public int Score { get; set; }
         public DateTime time { get; set; }
         public int MatchId { get; set; }
         public int PlayerId { get; set; }
         public int JudgeId { get; set; }
         public int NoOfConfirmation { get; set; }
-
+        public int NoOfClickIn3Sec { get; set; } // defult is 1
         public ScoreDto()
         {
             time = DateTime.UtcNow;
